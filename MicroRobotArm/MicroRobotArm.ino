235852051549d66f9635775beb4c6e3a440cbafa
T-Kuhn <tkuhn.mpu@gmail.com>
2018-09-10 23:10:39 +0900

    ISP speed test with I/O operations and double math (float math makes the ISR crash)

--- MicroRobotArm/MicroRobotArm.ino
+++ MicroRobotArm/MicroRobotArm.ino
@@ -8,31 +8,57 @@
  This example code is in the public domain.
  */
 
-// Stop button is attached to PIN 0 (IO0)
-#define BTN_STOP_ALARM 0
+#define STEP 2
+#define DIR 4
+
+// TestPins
+#define TP1 16
+#define TP2 17
+#define TP3 18
+#define TP4 19
+
+#define EXECUTING_ISR_CODE 5
 
 hw_timer_t *timer = NULL;
 volatile SemaphoreHandle_t timerSemaphore;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 volatile uint32_t isrCounter = 0;
-volatile uint32_t lastIsrAt = 0;
-
-int step = 2;
-int dir = 4;
 
 void IRAM_ATTR onTimer()
 {
-  // Increment the counter and set the time of ISR
+  digitalWrite(EXECUTING_ISR_CODE, HIGH);
+
+  digitalWrite(TP1, HIGH);
+  digitalWrite(TP2, HIGH);
+  digitalWrite(TP3, HIGH);
+
+  digitalWrite(TP1, LOW);
+  digitalWrite(TP2, LOW);
+  digitalWrite(TP3, LOW);
+
+  // Increment the counter of the ISR
   portENTER_CRITICAL_ISR(&timerMux);
   isrCounter++;
-  lastIsrAt = millis();
   portEXIT_CRITICAL_ISR(&timerMux);
+
+  if (cos((double)isrCounter * 0.01) > 0.4)
+  {
+    digitalWrite(TP4, LOW);
+  }
+  else
+  {
+    digitalWrite(TP4, HIGH);
+  }
+
   // Give a semaphore that we can check in the loop
   xSemaphoreGiveFromISR(timerSemaphore, NULL);
+
   // It is safe to use digitalRead/Write here if you want to toggle an output
-  int currentStepLevel = digitalRead(step);
-  digitalWrite(step, !currentStepLevel);
+  int currentStepLevel = digitalRead(STEP);
+  digitalWrite(STEP, !currentStepLevel);
+
+  digitalWrite(EXECUTING_ISR_CODE, LOW);
 }
 
 void setup()
@@ -40,10 +66,16 @@ void setup()
   Serial.begin(115200);
 
   // Set BTN_STOP_ALARM to input mode
-  pinMode(BTN_STOP_ALARM, INPUT);
-  pinMode(step, OUTPUT);
-  pinMode(dir, OUTPUT);
-  digitalWrite(dir, HIGH);
+  pinMode(STEP, OUTPUT);
+  pinMode(DIR, OUTPUT);
+  pinMode(EXECUTING_ISR_CODE, OUTPUT);
+
+  pinMode(TP1, OUTPUT);
+  pinMode(TP2, OUTPUT);
+  pinMode(TP3, OUTPUT);
+  pinMode(TP4, OUTPUT);
+
+  digitalWrite(DIR, HIGH);
 
   // Create semaphore to inform us when the timer has fired
   timerSemaphore = xSemaphoreCreateBinary();
@@ -56,9 +88,9 @@ void setup()
   // Attach onTimer function to our timer.
   timerAttachInterrupt(timer, &onTimer, true);
 
-  // Set alarm to call onTimer function every seconds (value in microseconds).
+  // Set alarm to call onTimer function every x microseconds.
   // Repeat the alarm (third parameter)
-  timerAlarmWrite(timer, 700, true); // 1000000
+  timerAlarmWrite(timer, 700, true);
 
   // Start an alarm
   timerAlarmEnable(timer);
@@ -66,32 +98,31 @@ void setup()
 
 void loop()
 {
+  delay(1000);
+
   // If Timer has fired
   if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE)
   {
-    uint32_t isrCount = 0, isrTime = 0;
-    // Read the interrupt count and time
+    uint32_t isrCount = 0;
+
+    // Read the interrupt count
     portENTER_CRITICAL(&timerMux);
     isrCount = isrCounter;
-    isrTime = lastIsrAt;
     portEXIT_CRITICAL(&timerMux);
-    // Print it
-    Serial.print("onTimer no. ");
-    Serial.print(isrCount);
-    Serial.print(" at ");
-    Serial.print(isrTime);
-    Serial.println(" ms");
-  }
 
-  // If button is pressed
-  if (digitalRead(BTN_STOP_ALARM) == LOW)
-  {
-    // If timer is still running
-    if (timer)
-    {
-      // Stop and free timer
-      timerEnd(timer);
-      timer = NULL;
-    }
+    // Print it
+    //Serial.print("cnt: ");
+    //Serial.println(isrCount);
   }
 }
+
+// MEMO:
+//
+// TODO:
+// - implement that sin thing.
+//
+// DOING:
+// - implement a mechanism to see how much time we spend in the ISR.
+//
+// DONE:
+//
