5cb1315060eb532435509059f0712798fff4e8cd
T-Kuhn <tkuhn.mpu@gmail.com>
2018-10-24 22:22:04 +0900

    Added Unity robot arm simulation environment.

---
+++ ESP32-RobotArm-IK-Eval/Assets/JointSetter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointSetter : MonoBehaviour
+{
+    [SerializeField] GameObject joint1;
+    [SerializeField] GameObject joint2;
+    [SerializeField] GameObject joint3;
+    [SerializeField] GameObject joint4;
+
+    [SerializeField] Vector3 joint1Rot;
+    [SerializeField] Vector3 joint2Rot;
+    [SerializeField] Vector3 joint3Rot;
+    [SerializeField] Vector3 joint4Rot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        joint1.transform.localRotation = Quaternion.Euler(joint1Rot * 180f / Mathf.PI);
+        joint2.transform.localRotation = Quaternion.Euler(joint2Rot * 180f / Mathf.PI);
+        joint3.transform.localRotation = Quaternion.Euler(joint3Rot * 180f / Mathf.PI);
+        joint4.transform.localRotation = Quaternion.Euler(joint4Rot * 180f / Mathf.PI);
+    }
+}
