05f697a636ff222a3fc4ff12519513d2c0a5ee8c
T-Kuhn <tkuhn.mpu@gmail.com>
2018-10-28 20:51:57 +0900

    Added UniRx.

---
+++ MicroRobotArm-Unity/Assets/Scripts/TransparenceSetter.cs
@@ -0,0 +1,30 @@
+﻿// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//  TransparenceSetter.cs
+//  Author: T-Kuhn.
+//  Sapporo, September, 2018. Released into the public domain.
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace RobotArm
+{
+    public class TransparenceSetter : MonoBehaviour
+    {
+        public bool MakeTransparent { set { _makeTransparent = value; } }
+        [SerializeField] bool _makeTransparent;
+
+        [SerializeField] Renderer[] _targets;
+
+        void Update()
+        {
+            if (_makeTransparent)
+            {
+
+
+            }
+        }
+    }
+}
